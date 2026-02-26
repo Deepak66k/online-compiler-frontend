5ec48d8a2735a368dfa9916db873d48b011fa89c
DEEPAK <deepgniit90@gmail.com>
2026-02-26 19:28:31 +0530

    fix url app.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     setLoading(true);
     setOutput(`> Initializing ${language} runtime...\n`);
     try {
-      const response = await fetch("http://127.0.0.1:8000/run", {
+      const response = await fetch("https://online-compiler-backend-t5k9.onrender.com/run", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ code, language: language.toLowerCase() })
